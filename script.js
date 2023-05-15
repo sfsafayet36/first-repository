3bb63072d2694d020862361cb9c4bb48083f8743
sf.safayet36@gmail.com <sf.safayet36@gmail.com>
2023-05-15 18:11:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+/*Data types are two types */
+
+/* 1: primitive and 
+2 : reference */
+
+/*primitive data types example : number,null,string,bolean,undefined, symbol */
+
+/*reference data types example : object and array */
+
+
+
+/* let a = 20;
+let b = 30;
+
+console.log(a);
+console.log(b);
+console.log('The value is a + b is', a+b) */
+
+
+
+/* creat function */
+
+/* function peoples(name, ages, address, work){
+  
+  console.log(name + ' is a good boy age ' + ages + ' lives in ' + address + ' work as ' + work);
+  
+  
+}
+peoples('Safu', 18, 'America', 'Programmer')
+peoples('Talha', 17, 'Bangladesh','Student')
+peoples('Shakib', 14, 'Turkey', 'content creator')
+peoples('Sourov', 19, 'Afghanistan', 'Businessmen')
+*/
+
+
+/* function workers(names, salary, day){
+  
+  return (names + salary + day)
+  
+}
+
+x = workers('Safu ', '10000 ' , '30')
+
+console.log(x)  */
+
+
+
+/* creating object */
+
+/* const car = {
+  name:'Toyota',
+  model:'Harrier',
+  color:'Black',
+  weight:'850kg',
+  
+  start: function(){
+    console.log('car is started')
+    
+  },
+  
+  stop: function(name){
+    console.log('car is stoped');
+   
+  }
+  
+  
+}
+
+console.log(car.weight);
+
+car.start()
+
+
+
+
+function clickme(){
+ 
+document.getElementById('btn').innerHTML = Date()
+  
+  
+}
+*/
+
+let s = 'hello guys i am safu'
+
+console.log(s.slice(6, 10));
+
+console.log(s.substr(6, 4));
---
+++ scripted.js
@@ -0,0 +1,11 @@
+function navClick(){
+  document.getElementById('ul-responsive').style.transform='translateX(-20%)'
+  
+document.getElementById('ul-responsive').style.transition='0.5s ease'
+}
+
+function navOver(){
+  document.getElementById('ul-responsive').style.transform='translateX(-100%)'
+  
+  
+}
